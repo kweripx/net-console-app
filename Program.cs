cfe9166e9ac5c827640969c9647bd5f03deb172a
vitor.qz <vitor.queiroz@octoti.com.br>
2022-04-26 10:57:35 -0300

    chore: created the baseline of HttpRequest

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+
+namespace ConsoleAppTeste
+{
+  class Program
+  {
+    static readonly HttpClient _client = new HttpClient();
+    static async Task Main(string[] args)
+    {
+      try
+      {
+        HttpResponseMessage response = await _client.GetAsync("https://api.nobelprize.org/v1/prize.json?category=");
+      }
+      catch (HttpRequestException e)
+      {
+        Console.WriteLine("\nException Caught");
+        Console.WriteLine("Message: {0}", e.Message);
+      }
+    }
+  }
+}
