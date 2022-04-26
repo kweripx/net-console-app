02deabf2e113e7bad4909320c926f84a5c5ce9a7
kweripx <vitor.hugoqueiroz11@gmail.com>
2022-04-26 16:43:40 -0300

    refactor: turning api request into a method

--- Program.cs
+++ Program.cs
@@ -2,43 +2,51 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Threading;
 using nobelPrizes;
 namespace ConsoleAppTeste
 {
   class Program
   {
     static readonly HttpClient _client = new HttpClient();
-    static async Task Main(string[] args)
+
+
+    static async Task GetData(string category, long year)
     {
-      try
+      HttpResponseMessage response = await _client.GetAsync($"https://api.nobelprize.org/v1/prize.json?category={category}&year={year}");
+      var content = await response.Content.ReadAsStringAsync();
+      var nobelPrize = JsonConvert.DeserializeObject<RootPrize>(content);
+
+      foreach (var item in nobelPrize.Prizes)
       {
-        Console.WriteLine("Insert a category: ");
-        var category = Console.ReadLine().ToLower();
-        Console.WriteLine("Insert a year: ");
-        var year = Console.ReadLine();
-        HttpResponseMessage response = await _client.GetAsync($"https://api.nobelprize.org/v1/prize.json?category={category}&year={year}");
-        var content = await response.Content.ReadAsStringAsync();
-
-
-        var nobelPrize = JsonConvert.DeserializeObject<RootPrize>(content);
-
-        foreach (var item in nobelPrize.Prizes)
+        foreach (var i in nobelPrize.Prizes)
         {
-          foreach (var i in nobelPrize.Prizes)
-          {
-            Console.WriteLine($"\n{item.Category}\n");
-            Console.WriteLine($"{item.Year}\n");
-            Console.WriteLine($"{i.Laureates}\n");
-            string log = content.ToString();
-            await File.WriteAllTextAsync("log.txt", log);
-          }
+          Console.WriteLine($"\n{item.Category}\n");
+          Console.WriteLine($"{item.Year}\n");
+          Console.WriteLine($"{i.Laureates}\n");
+          string log = content.ToString();
+          await File.WriteAllTextAsync("log.txt", log);
         }
       }
-      catch (HttpRequestException e)
-      {
-        Console.WriteLine("\nException Caught");
-        Console.WriteLine("Message: {0}", e.Message);
-      }
+      Thread.Sleep(TimeSpan.FromSeconds(15));
     }
+    static void Main()
+    {
+      GetData();
+      // try
+      // {
+      //   Console.WriteLine("Insert a category: ");
+      //   var category = Console.ReadLine().ToLower();
+      //   Console.WriteLine("Insert a year: ");
+      //   var year = Console.ReadLine();
+
+      // }
+      // catch (HttpRequestException e)
+      // {
+      //   Console.WriteLine("\nException Caught");
+      //   Console.WriteLine("Message: {0}", e.Message);
+      // }
+    }
+
   }
 }
